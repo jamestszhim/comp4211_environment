b311c5b674aed97d985de6e323ee95714ad0fdc3
Tsz Him Cheung <tszhimcheung@Tszs-MacBook-Pro.local>
2020-03-13 22:53:31 +0800

    wrap up in build env

--- build_environment.sh
+++ build_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install virtualenv --user
-~/.local/virtualenv -p python3 ~/comp4211_env
+~/.local/bin/virtualenv -p python3 ~/comp4211_env
 source ~/comp4211_env/bin/activate
 pip install -r requirements.txt
