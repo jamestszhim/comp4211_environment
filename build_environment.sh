84fe37d3eebb655b5637d16378f24147bd102b50
Tsz Him Cheung <tszhimcheung@Tszs-MacBook-Pro.local>
2020-03-14 11:27:32 +0800

    change to csh

--- build_environment.sh
+++ build_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install virtualenv --user
 ~/.local/bin/virtualenv -p python3 ~/comp4211_env
-source ~/comp4211_env/bin/activate
+source ~/comp4211_env/bin/activate.csh
 pip install -r requirements.txt
